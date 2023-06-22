c607a2d6091b0d12f517d2fc4131edae076665b4
nation <hananiyusuph98@gmail.com>
2023-06-22 18:54:47 +0300

    ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,22 +13,22 @@ const Home = ({token}) => {
     }
     return (
         <>
-            <div className="px-10 py-5">
-                <div className="bg-white max-w-md p-3 rounded ">
+            <div className="py-5 px-3 md:px-10 md:py-5">
+                <div className="bg-white max-w-md mx-auto p-3 rounded ">
                     <div className="flex justify-between">
                         <div className="flex items-center">
-                            <h3 className="bg-white rounded-full shadow-xl  p-4 w-14 "><FaRegUser className="h-6 w-6" /></h3>
+                            <h3 className="bg-white rounded-full shadow-xl p-2  md:p-4 md:w-14 "><FaRegUser className="h-4 w-4 md:h-6 md:w-6" /></h3>
                             <span className=" ml-5">
-                                <h1 className="uppercase text-md text-emerald-800 font-semibold">{token.user.user_metadata.name}</h1>
-                                <h3 className="uppercase text-sm text-emerald-800 font-semibold">{token.user.user_metadata.phone}</h3>
+                                <h1 className="uppercase text-xs md:text-md text-emerald-800 font-semibold">{token.user.user_metadata.name}</h1>
+                                <h3 className="uppercase text-xs md:text-sm text-emerald-800 font-semibold">{token.user.user_metadata.phone}</h3>
                             </span> 
                         </div>
                         <button className="btn-pri " onClick={Logout}>
-                            <h3 className="ml-2"><BiLogOut className="h-6 w-6" /></h3>
+                            <h3 className="ml-2"><BiLogOut className="h-4 w-4 md:h-6 md:w-6" /></h3>
                             logout
                         </button>
                     </div>
-                    <h2 className="uppercase text-xs ml-2 mt-2 text-emerald-800 font-semibold">{token.user.user_metadata.user}</h2>
+                    <h2 className="uppercase text-xs  md:ml-2 md:mt-2 text-emerald-800 font-semibold">{token.user.user_metadata.user}</h2>
                 </div>
             </div>
         </>
